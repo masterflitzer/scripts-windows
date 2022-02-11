9537923471526da6c24d5b5289b13cba1a6d1937
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2022-02-11 01:58:15 +0100

    add script

---
+++ Users/david/bin/beep-on-lock.ps1
@@ -0,0 +1,27 @@
+[CmdletBinding()]
+param ([switch]$beep)
+
+function IsSuspended {
+    $process = Get-Process lockapp
+    $threads = $process.Threads
+    $thread = $threads[0]
+    return ($thread.ThreadState -eq "wait") -and ($thread.WaitReason -eq "suspended")
+}
+
+function MyDate {
+    return Get-Date -DisplayHint DateTime -Format "yyyy-MM-dd HH:mm:ss"
+}
+
+Write-Host -ForegroundColor Blue "Exit the script with Ctrl+C"
+
+$state = $true
+
+while ($true) {
+    if (!$state -eq $(IsSuspended)) {
+        $state = !$state
+        if ($beep) { [System.Media.SystemSounds]::Beep.Play() }
+        if ($state) { Write-Host -ForegroundColor Red "$(MyDate) - Screen unlocked!" }
+        else { Write-Host -ForegroundColor Green "$(MyDate) - Screen locked!" }
+    }
+    Start-Sleep -Seconds 1
+}
